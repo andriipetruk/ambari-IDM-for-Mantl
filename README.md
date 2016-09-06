c1196046969c66504be95cde2fcb3d3c9dd5abb6
Andrii Petruk <andrey.petruk@gmail.com>
2016-09-06 18:49:30 +0300

    upload

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # ambari-IDM-for-Mantl
+All the work on pangea integration
+
+## Purpose
+
+Implement SSO and additional security level to mantl-pangea solution  
+
+
+##REQUIREMENTS:
+####Project components:
+```
+Ansible 1.9.2
+Docker
+Ambari 2.4.0
+Knox 0.9.1
+
+```
+i111
+1
+
+iddd
+
+##QUICK START:
+
+
+######Solution architect
+![alt tag](https://github.com/andriipetruk/ambari-IDM-for-Mantl/blob/master/mantl-pangea.png?raw=true)
---
+++ docker-knox/bootstrap-knox.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+# knox bootstrap
+
+mkdir -p /var/log/knox
+java -jar $GATEWAY_HOME/bin/ldap.jar $GATEWAY_HOME/conf &>/var/log/knox/ldap.out &
+
+cd $GATEWAY_HOME
+mkdir -p conf/security
+echo '#1.0#' > conf/security/master
+
+# Make sure to read and understand the "Persisting the Master Secret" paragraph : http://knox.incubator.apache.org/books/knox-0-3-0/knox-0-3-0.html
+# This is a test/development container - change this in production
+echo 'UjFkVDZNS0N5Yzg9Ojp1WVNwREtFeG9KcHN1QjFYU1JDRkh3PT06OldTaUdOT1U5RUw0ejZ5SUM0VE5LMVE9PQ==' >> conf/security/master
+bin/gateway.sh setup root
+bin/gateway.sh start
+
+
+
+if [[ $1 == "-d" ]]; then
+  while true; do sleep 1000; done
+fi
+
+if [[ $1 == "-bash" ]]; then
+  /bin/bash
+fi
